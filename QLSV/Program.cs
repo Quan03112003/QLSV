ce8d107b0db564e9dc5ee73fb885b181aaa792f4
quan0 <quan0@QUAN03112003>
2025-03-28 14:36:42 +0700

    Add project files.

---
+++ QLSV/Program.cs
@@ -0,0 +1,25 @@
+﻿// Program.cs
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+  
+        List<Student> students = new List<Student>();
+
+    
+        students.Add(new Student(1, "Nguyễn Văn A", 20, 8.5));
+        students.Add(new Student(2, "Trần Thị B", 22, 7.2));
+        students.Add(new Student(3, "Lê Quang C", 21, 9.0));
+        students.Add(new Student(4, "Phạm Minh D", 23, 6.8));
+
+        
+        Console.WriteLine("Danh sách sinh viên:");
+        foreach (var student in students)
+        {
+            student.Display();
+        }
+    }
+}
---
+++ QLSV/Student.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+public class Student
+{
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public double GPA { get; set; }
+
+
+    public Student(int id, string name, int age, double gpa)
+    {
+        Id = id;
+        Name = name;
+        Age = age;
+        GPA = gpa;
+    }
+
+
+    public void Display()
+    {
+        Console.WriteLine($"ID: {Id}, Tên: {Name}, Tuổi: {Age}, GPA: {GPA:F2}");
+    }
+}
